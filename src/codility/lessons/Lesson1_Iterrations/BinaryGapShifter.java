472542c85c0f3ebd371f8cb1c4fdeb0fe7220eda
Dragon Bean <yp2211@aliyun.com>
2021-05-27 17:14:43 +0900

    20210527

---
+++ src/codility/lessons/Lesson1_Iterrations/BinaryGapShifter.java
@@ -0,0 +1,33 @@
+package codility.lessons.Lesson1_Iterrations;
+
+/**
+ * Created by peng.yang on ${Date}
+ */
+public class BinaryGap {
+
+    public static int solution(int N) {
+        int goal = 0;
+
+        String binaryN = Integer.toBinaryString(N);
+        char[] charsBinaryN = binaryN.toCharArray();
+
+        int counter = 0;
+        boolean counterSwitch = false;
+        for (char aCharsBinaryN : charsBinaryN) {
+            if (aCharsBinaryN == '0') {
+                counterSwitch = true;
+                counter++;
+            } else if (aCharsBinaryN == '1') {
+                if (counterSwitch) {
+                    counterSwitch = false;
+                    if (counter > goal) {
+                        goal = counter;
+                    }
+                    counter = 0;
+                }
+            }
+        }
+
+        return goal;
+    }
+}
---
+++ test/codility/lessons/Lesson1_Iterrations/BinaryGapShifterTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by peng.yang on ${Date}
+ */
+class BinaryGapShifterTest {
+
+}
