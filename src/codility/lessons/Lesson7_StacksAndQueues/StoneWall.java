39ce4d87d84cadcac912ada5b96a8e2ed53f321c
Dragon Bean <yp2211@yahoo.co.jp>
2021-09-10 23:26:05 +0900

    Codility Lesson7: Stacks and Queues

--- src/codility/lessons/Lesson7_StacksAndQueues/StoneWall.java
+++ src/codility/lessons/Lesson7_StacksAndQueues/StoneWall.java
@@ -4,18 +4,18 @@ public class StoneWall {
     public int solution(int[] H) {
         java.util.Stack<Integer> stack = new java.util.Stack<>();
         int result = 0;
-        for (int i = 0; i < H.length; i++) {
+        for (int newWallHeight : H) {
             while (!stack.isEmpty()) {
                 Integer preStone = stack.pop();
-                if (preStone == H[i]) {
+                if (preStone == newWallHeight) {
                     // same height should be merged, so it is enough that we push one stone back to stack
                     stack.push(preStone);
                     break;
-                } else if (H[i] > preStone) {
+                } else if (newWallHeight > preStone) {
                     // new wall is higher, so we should push it into the stack,
                     // because we need one more stone to build higher wall
                     stack.push(preStone);
-                    stack.push(H[i]);
+                    stack.push(newWallHeight);
                     break;
                 } else {
                     // In this case, previous stone is higher,so we can not merge the new one to previous stone,
@@ -32,7 +32,7 @@ public class StoneWall {
             //   maybe the new wall is the first wall,
             //   maybe the new wall is so low that we have popped all stones from the stack yet.
             // Anyway, we should push it into stack, and go to next wall.
-            if (stack.isEmpty()) stack.push(H[i]);
+            if (stack.isEmpty()) stack.push(newWallHeight);
         }
         // Finally, the number of blocks is we recorded ones, and the number of stones which are still in our stack.
         return result + stack.size();
